c64fc0b9c9fc247912a9db1a04e9bb2294ef69d8
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-11 18:56:59 +0200

    style(index.ts) prettier compliant

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 const defaultOptions = {
-  replaceBy: "*",
   offsetLeft: 2,
   offsetRight: 2,
+  replaceBy: "*"
 };
 
-export interface Options {
-  replaceBy?: string,
-  offsetLeft?: number,
-  offsetRight?: number,
+export interface IOptions {
+  replaceBy?: string;
+  offsetLeft?: number;
+  offsetRight?: number;
 }
 
-export default function (value: string, options?: Options) {
-  const opt = {...defaultOptions, ...options};
-  const regex = new RegExp("(?<=\\w{" + opt.offsetLeft + "})\\w(?=\\w{" + opt.offsetRight + "})", "g");
+export default function(value: string, options?: IOptions) {
+  const opt = { ...defaultOptions, ...options };
+  const regex = new RegExp(
+    "(?<=\\w{" + opt.offsetLeft + "})\\w(?=\\w{" + opt.offsetRight + "})",
+    "g"
+  );
   return value.replace(regex, opt.replaceBy);
-};
+}
