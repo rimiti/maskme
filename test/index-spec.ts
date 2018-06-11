6ca69f4e3e19cf47a88745f685310f414f090acf
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-11 14:06:18 +0200

    chore(test/index-spec.ts) test created

--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,6 +1,15 @@
 import test from "ava";
-import Markme from "../src";
+import Maskme from "../src";
 
-test("Should mask credit card numbers", (t) => {
-  t.is(true, true);
+test("Should mask credit card numbers \"4242424242424242\" => \"4242********4242\" (default configuration)", (t) => {
+  t.is(Maskme("4242424242424242"), "4242********4242");
 });
+
+test("Should mask credit card numbers \"4242424242424242\" => \"4242--------4242\"", (t) => {
+  t.is(Maskme("4242424242424242", {replaceBy: "-"}), "4242--------4242");
+});
+
+test("Should mask credit card numbers \"4242424242424242\" => \"4242xxxxxxxx4242\"", (t) => {
+  t.is(Maskme("4242424242424242", {replaceBy: "x"}), "4242xxxxxxxx4242");
+});
+
