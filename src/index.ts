8e40c77198609c55414fc3cef26324a99d831d55
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-11 18:19:30 +0200

    feat(index.ts) wip

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,58 @@
 const defaultOptions = {
   replaceBy: "*",
-  type: "default",
+  type: "middle",
   numberOfChars: 4,
 };
 
-export default function (value: string, options: Object = {}) {
+export interface Options {
+  replaceBy?: string,
+  type?: Type,
+  numberOfChars?: number,
+}
+
+export enum Type {
+  middle = "middle",
+  end = "end",
+  start = "start",
+}
+
+export default function (value: string, options?: Options) {
   const opt = {...defaultOptions, ...options};
-  let regex = new RegExp("(?<=\\d{" + opt.numberOfChars + "})\\d(?=\\d{" + opt.numberOfChars + "})", "g");
+
+  let regex: RegExp;
+  if (opt.type === Type.middle) {
+    if (opt.numberOfChars > value.length) {
+      let toReplace = "";
+      for (let i = 0; i < opt.numberOfChars; i += 1) {
+        toReplace += opt.replaceBy.toString()
+      }
+      return toReplace;
+    }
+
+
+    // Impair
+    if (((value.length - opt.numberOfChars) / 2) % 2) {
+
+      const middle = (value.length - opt.numberOfChars) / 2;
+      const left = value.substring(0, middle);
+      const right = value.substring(middle + opt.numberOfChars, value.length);
+
+      let toReplace = "";
+      for (let i = 0; i < opt.numberOfChars; i += 1) {
+        toReplace += opt.replaceBy.toString()
+      }
+      return left + toReplace + right;
+    }
+
+
+    return;
+
+  } else if (opt.type === Type.start) {
+    regex = new RegExp("\\w(?=\\w{" + opt.numberOfChars + "})", "g");
+  } else if (opt.type === Type.end) {
+    regex = new RegExp("(?<=\\w{" + opt.numberOfChars + "})\\w", "g");
+  } else {
+    throw new TypeError(`Unknown type "${opt.type}" (available types: ${Type})`)
+  }
   return value.replace(regex, opt.replaceBy);
 };
