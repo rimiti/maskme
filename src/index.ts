4395d52767a57916f7a5321e2b7646884881737d
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-11 18:23:33 +0200

    feat(index.ts) hard refact

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
 const defaultOptions = {
   replaceBy: "*",
-  type: "middle",
   numberOfChars: 4,
 };
 
 export interface Options {
   replaceBy?: string,
-  type?: Type,
   numberOfChars?: number,
-}
-
-export enum Type {
-  middle = "middle",
-  end = "end",
-  start = "start",
 }
 
 export default function (value: string, options?: Options) {
   const opt = {...defaultOptions, ...options};
-
-  let regex: RegExp;
-  if (opt.type === Type.middle) {
-    regex = new RegExp("(?<=\\w{"+ opt.numberOfChars + "})\\w(?=\\w{" + opt.numberOfChars + "})", "g");
-  } else if (opt.type === Type.start) {
-    regex = new RegExp("\\w(?=\\w{" + opt.numberOfChars + "})", "g");
-  } else if (opt.type === Type.end) {
-    regex = new RegExp("(?<=\\w{" + opt.numberOfChars + "})\\w", "g");
-  } else {
-    throw new TypeError(`Unknown type "${opt.type}" (available types: ${Type})`)
-  }
+  const regex = new RegExp("(?<=\\w{" + opt.numberOfChars + "})\\w(?=\\w{" + opt.numberOfChars + "})", "g");
   return value.replace(regex, opt.replaceBy);
 };
