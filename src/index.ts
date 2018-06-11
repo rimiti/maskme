d26fdf59eca2472192817775a70af8f376506e27
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-11 14:07:42 +0200

    feat(index.ts) logic implemented

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-export default function () {
-  // TODO: Implement here
+const defaultOptions = {
+  replaceBy: "*",
+  type: "default",
+  numberOfChars: 4,
 };
 
+export default function (value: string, options: Object = {}) {
+  const opt = {...defaultOptions, ...options};
+  let regex = new RegExp("(?<=\\d{" + opt.numberOfChars + "})\\d(?=\\d{" + opt.numberOfChars + "})", "g");
+  return value.replace(regex, opt.replaceBy);
+};
