19930e7b6a71d636ffad8f9d9ac943b0a111939d
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-06-11 18:39:50 +0200

    chore(test/index-spec.ts) code coverage 100%

--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,42 +1,30 @@
 import test from "ava";
-import Maskme, {Type} from "../src";
+import Maskme from "../src";
 
-test("Should mask credit card numbers \"4242424242424242\" => \"4242********4242\" (default configuration)", (t) => {
-  t.is(Maskme("4242424242424242"), "4242********4242");
+test("Shouldn't hide 2 first and 2 last chars \"4242424242424242\" => \"42************42\" (default configuration)", (t) => {
+  t.is(Maskme("4242424242424242"), "42************42");
 });
 
-test("Should mask credit card numbers \"4242424242424242\" => \"4242--------4242\"", (t) => {
-  t.is(Maskme("4242424242424242", {replaceBy: "-"}), "4242--------4242");
+test("Shouldn't hide 4 first and 4 last chars \"4242424242424242\" => \"4242xxxxxxxx4242\"", (t) => {
+  t.is(Maskme("4242424242424242", {replaceBy: "x"}), "42xxxxxxxxxxxx42");
 });
 
-test("Should mask credit card numbers \"4242424242424242\" => \"4242xxxxxxxx4242\"", (t) => {
-  t.is(Maskme("4242424242424242", {replaceBy: "x"}), "4242xxxxxxxx4242");
+test("Shouldn't hide 2 last chars \"4242424242424242\" => \"**************42\"", (t) => {
+  t.is(Maskme("4242424242424242", {offsetLeft: 0,}), "**************42");
 });
 
-test("Should mask all chars except 4 last  \"4242424242424242\" => \"************4242\"", (t) => {
-  t.is(Maskme("4242424242424242", {type: Type.start}), "************4242");
+test("Shouldn't hide 2 first chars \"4242424242424242\" => \"42**************\"", (t) => {
+  t.is(Maskme("4242424242424242", {offsetRight: 0}), "42**************");
 });
 
-test("Should mask all chars except 2 last \"4242424242424242\" => \"**************42\"", (t) => {
-  t.is(Maskme("4242424242424242", {type: Type.start, numberOfChars: 2}), "**************42");
+test("Should hide all chars \"4242424242424242\" => \"****************\"", (t) => {
+  t.is(Maskme("4242424242424242", {offsetRight: 0, offsetLeft: 0}), "****************");
 });
 
-test("Should mask all chars except 4 first \"4242424242424242\" => \"4242************\"", (t) => {
-  t.is(Maskme("4242424242424242", {type: Type.end}), "4242************");
+test("Shouldn't hide 6 first and 5 last chars \"4242424242424242\" => \"424242*****24242\"", (t) => {
+  t.is(Maskme("4242424242424242", {offsetLeft: 6, offsetRight: 5}), "424242*****24242");
 });
 
-test("Should mask all chars except 2 first \"4242424242424242\" => \"42**************\"", (t) => {
-  t.is(Maskme("4242424242424242", {type: Type.end, numberOfChars: 2}), "42**************");
-});
-
-test("Should mask nothing \"42\" => \"42\" (default configuration)", (t) => {
-  t.is(Maskme("42"), "42");
-});
-
-test("Should mask nothing \"42\" => \"42\"", (t) => {
-  t.is(Maskme("42", {type: Type.start}), "42");
-});
-
-test("Should mask nothing \"42\" => \"42\"", (t) => {
-  t.is(Maskme("42", {type: Type.end}), "42");
+test("Shouldn't hide nothing \"4242424242424242\" => \"4242424242424242\"", (t) => {
+  t.is(Maskme("4242424242424242", {offsetLeft: 30, offsetRight: 40}), "4242424242424242");
 });
