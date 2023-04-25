430a3185b41376e1a575b93071784af6cd2518e1
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2023-04-25 10:32:46 +0200

    style: lint:fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 const defaultOptions = {
   offsetLeft: 2,
   offsetRight: 2,
-  replaceBy: "*"
+  replaceBy: "*",
 };
 
 export interface IOptions {
@@ -10,7 +10,7 @@ export interface IOptions {
   offsetRight?: number;
 }
 
-export default function(value: string, options?: IOptions) {
+export default function (value: string, options?: IOptions) {
   const opt = { ...defaultOptions, ...options };
   const regex = new RegExp(
     "(?<=\\w{" + opt.offsetLeft + "})\\w(?=\\w{" + opt.offsetRight + "})",
--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,37 +1,37 @@
 import test from "ava";
 import Maskme from "../src";
 
-test('Shouldn\'t hide 2 first and 2 last chars "4242424242424242" => "42************42" (default configuration)', t => {
+test('Shouldn\'t hide 2 first and 2 last chars "4242424242424242" => "42************42" (default configuration)', (t) => {
   t.is(Maskme("4242424242424242"), "42************42");
 });
 
-test('Shouldn\'t hide 4 first and 4 last chars "4242424242424242" => "42xxxxxxxxxxxx42"', t => {
+test('Shouldn\'t hide 4 first and 4 last chars "4242424242424242" => "42xxxxxxxxxxxx42"', (t) => {
   t.is(Maskme("4242424242424242", { replaceBy: "x" }), "42xxxxxxxxxxxx42");
 });
 
-test('Shouldn\'t hide 2 last chars "4242424242424242" => "**************42"', t => {
+test('Shouldn\'t hide 2 last chars "4242424242424242" => "**************42"', (t) => {
   t.is(Maskme("4242424242424242", { offsetLeft: 0 }), "**************42");
 });
 
-test('Shouldn\'t hide 2 first chars "4242424242424242" => "42**************"', t => {
+test('Shouldn\'t hide 2 first chars "4242424242424242" => "42**************"', (t) => {
   t.is(Maskme("4242424242424242", { offsetRight: 0 }), "42**************");
 });
 
-test('Should hide all chars "4242424242424242" => "****************"', t => {
+test('Should hide all chars "4242424242424242" => "****************"', (t) => {
   t.is(
     Maskme("4242424242424242", { offsetRight: 0, offsetLeft: 0 }),
     "****************"
   );
 });
 
-test('Shouldn\'t hide 6 first and 5 last chars "4242424242424242" => "424242*****24242"', t => {
+test('Shouldn\'t hide 6 first and 5 last chars "4242424242424242" => "424242*****24242"', (t) => {
   t.is(
     Maskme("4242424242424242", { offsetLeft: 6, offsetRight: 5 }),
     "424242*****24242"
   );
 });
 
-test('Shouldn\'t hide nothing "4242424242424242" => "4242424242424242"', t => {
+test('Shouldn\'t hide nothing "4242424242424242" => "4242424242424242"', (t) => {
   t.is(
     Maskme("4242424242424242", { offsetLeft: 30, offsetRight: 40 }),
     "4242424242424242"
